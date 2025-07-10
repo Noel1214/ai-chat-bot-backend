d08e8b25f7f44f50d38616d638588d450705df9c
Noel Sebu <noelsebu555@gmail.com>
2025-07-10 22:15:18 +0530

    cors implmented and route for testing added

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 // const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-dotenv.config();
+const aiChatRoute = require("./routes/ai-chat-route.js");
+
+// dotenv.config();
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+app.use("/ai", aiChatRoute);
 
 const PORT = 3001;
 app.listen(PORT, () => {
