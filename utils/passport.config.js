327cb1001160cf1beba340a021febdf54cc895db
Noel Sebu <noelsebu555@gmail.com>
2025-07-24 12:10:09 +0530

    passport config file created

---
+++ utils/passport.config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      return cb(null, profile);
+    }
+  )
+);
